ba33c2905dfe0b8f647200ce0d17ee583e9f7d81
Raihaan Riswan <mohamedraihaanmohamedriswan@gmail.com>
2025-12-17 17:39:05 +0530

    update file

--- output.php
+++ output.php
@@ -2,4 +2,18 @@
     <h2>
         Your total bill..
     </h2>
+    <br>
+    <?php
+        $bst = $_POST["bst"]
+        $ttlbst = $bst * 50;
+
+
+      ?>
+
+      <h3>
+        <?php
+        echo "$ttlbst";
+
+        ?>
+      </h3>
 </html>
