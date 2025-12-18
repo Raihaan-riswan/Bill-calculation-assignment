8e1adffe781935c3e03f08d24439bb71be1649c9
Raihaan Riswan <mohamedraihaanmohamedriswan@gmail.com>
2025-12-18 23:09:55 +0530

    update file

--- output.php
+++ output.php
@@ -4,16 +4,27 @@
     </h2>
     <br>
     <?php
-        $ttlbst = 
+        $ttlbst = "";
+        $ttlndl = "";
+        $ttlbrd = "";
+        $ttlmlk = "";
+        $ttlegg = "";
+        $ttldhl = "";
+        // this is the code for total biscuit
         $bst = $_POST["bst"];
         $ttlbst = $bst * 50;
+
+        // this is the code for total 
+        $ndl = $_POST["ndl"];
+        $ttlndl = $ndl * 100;
 
 
       ?>
 
       <h3>
         <?php
-        echo "$ttlbst";
+        echo "$ttlbst <br>";
+        echo "$ttlndl<br>";
 
         ?>
       </h3>
